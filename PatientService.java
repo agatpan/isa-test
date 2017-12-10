2b49112b30b7e41376b7884ffc744f43ddc365be
Agata Pankowska <pa.agatka@gmail.com>
2017-12-10 16:01:33 +0100

    change patients

--- PatientService.java
+++ PatientService.java
@@ -5,8 +5,12 @@ class PatientService {
     private final List<Patient> patients = new ArrayList<>();
 
     PatientService() {
-        this.patients.add(new Patient("Jan", "Kowalski", 11));
         this.patients.add(new Patient("Adam", "Nowak", 12));
+        this.patients.add(new Patient("Maria", "Nowakowska", 13));
+        this.patients.add(new Patient("Kasia", "Kowalska", 13));
+        this.patients.add(new Patient("Jan", "Nowak", 13));
+        this.patients.add(new Patient("Marcin", "Iksiński", 13));
+        this.patients.add(new Patient("Maria", "Nowakowska", 13));
         this.patients.add(new Patient("Maria", "Nowakowska", 13));
     }
 
